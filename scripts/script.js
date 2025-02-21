9199c364df76793be5e5017ae70bc7d91d8be3bf
Aluno Lab 03 <aluno.lab03@ifpe.edu>
2025-02-21 10:42:12 -0300

    ajustando lógica de logs

--- scripts/script.js
+++ scripts/script.js
@@ -7,15 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 200);
 
     // Eventos dos botões
-    document.getElementById("cota-ict").addEventListener("click", function () {
+    document.getElementById("cotas-ict").addEventListener("click", function () {
         window.open("./pages/cota-editor.html", "_blank");
     });
 
-    document.getElementById("delete-pn").addEventListener("click", function () {
-        window.open("./pages/delete-pn.html", "_blank");
+    document.getElementById("cotas-history").addEventListener("click", function () {
+        window.open("./pages/cotas-history.html", "_blank");
     });
 
-    document.getElementById("edit-pn").addEventListener("click", function () {
-        window.open("./pages/edit-pn.html", "_blank");
-    });
+
 });
